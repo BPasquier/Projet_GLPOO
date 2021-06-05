8de85c1a1aeb4bc2abceeb586d1658c7bf19d07e
Baptiste <baptiste.pasquier8@gmail.com>
2021-06-05 20:32:17 +0200

    Ajout fonction controller

--- controller/Sauce_Controller.py
+++ controller/Sauce_Controller.py
@@ -1,15 +1,29 @@
 import re
 
-import 
+from model.dao.member_dao import MemberDAO
 
 class SauceController :
-    def Create_Account():
-        
-    def Connexion():
 
-    def Create_Post():
+    def __init__(self, database_engine):
+        self._database_engine = database_engine
+
+    def Create_Account(self, Pseudo, Mdp):
+        with self._database_engine.new_session() as session:
+            MemberDAO(session).AddAccount(Pseudo, Mdp)
+
+    def Connexion(self, Pseudo, Mdp):
+        with self._database_engine.new_session() as session:
+            ListeMembres = MemberDAO(session).getAllAccount()
+            for membre in ListeMembres:
+                if membre[0] == Pseudo and membre[1] == Mdp:
+                    return # Return les posts
+
+    def Create_Post(self, Pseudo, texte):
+        with self._database_engine.new_session() as session:
+            MemberDAO(session).AddPost(Pseudo, texte)
 
     def Repondre():
+
 
     def Valider():
         
