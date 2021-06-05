ab1bb6d8902b06d9ef5321595912cd1fdff2315b
Baptiste <baptiste.pasquier8@gmail.com>
2021-06-05 18:52:00 +0200

    Ajouts template fonction controller

---
+++ controller/Sauce_Controller.py
@@ -0,0 +1,16 @@
+import re
+
+import 
+
+class SauceController :
+    def Create_Account():
+        
+    def Connexion():
+
+    def Create_Post():
+
+    def Repondre():
+
+    def Valider():
+        
+    
