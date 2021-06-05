fcdf115040c23fe83b4a1180b13653019fd4d776
Baptiste <baptiste.pasquier8@gmail.com>
2021-06-05 21:40:09 +0200

    Maj Controleur fonction

--- controller/Sauce_Controller.py
+++ controller/Sauce_Controller.py
@@ -1,6 +1,7 @@
 import re
 
-from model.dao.member_dao import MemberDAO
+from model.database import database
+
 
 class SauceController :
 
@@ -8,23 +9,19 @@ class SauceController :
         self._database_engine = database_engine
 
     def Create_Account(self, Pseudo, Mdp):
-        with self._database_engine.new_session() as session:
-            MemberDAO(session).AddUser(Pseudo, Mdp)
+        self.AddUser(Pseudo, Mdp)
 
     def Connexion(self, Pseudo, Mdp):
-        with self._database_engine.new_session() as session:
-            ListeMembres = MemberDAO(session).getAllUsers()
-            for membre in ListeMembres:
-                if membre.m_nickname == Pseudo and membre.m_password == Mdp:
-                    return membre.m_requestList
+        ListeMembres = self.getAllUsers()
+        for membre in ListeMembres:
+            if membre.m_nickname == Pseudo and membre.m_password == Mdp:
+                return membre.m_requestList
 
     def Create_Post(self, Pseudo, texte):
-        with self._database_engine.new_session() as session:
-            MemberDAO(session).AddRequest(Pseudo, texte)
+        self.AddRequest(Pseudo, texte)
 
     def Repondre():
 
 
     def Valider():
         
-    
