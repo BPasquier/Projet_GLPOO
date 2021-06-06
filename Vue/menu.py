7aebf3e9b833d190f813f88277781ff1058d1f65
PMalarde <malarde@et.esiea.fr>
2021-06-06 18:49:03 +0200

    Menu connexion quasi fonctionnel

--- Vue/menu.py
+++ Vue/menu.py
@@ -12,30 +12,127 @@ from PySide6.QtUiTools import QUiLoader
 import time
 
 
+class Dialog(QDialog):
+
+    def __init__(self):
+        super().__init__()
 
 
-def create_User(self):
-    loader = QUiLoader()
-    inscription = loader.load("Inscription.ui", None)
-    #window.pushButton.clicked.connect(lambda: print("OUI"))
-
-    inscription.show()
+        self.create_menu()
 
 
-def connect_User(self):
-    loader = QUiLoader()
-    connexion = loader.load("Connexion.ui", None)
-    #window.pushButton.clicked.connect(lambda: print("OUI"))
+    def create_menu(self):
 
-    connexion.show()
+        self._menu_bar = QMenuBar()
+
+        self._file_menu = QMenu("CrowdSaucing", self)
+        self._exit_action = self._file_menu.addAction("E&xit")
+        self._menu_bar.addMenu(self._file_menu)
+
+        self._exit_action.triggered.connect(self.accept)
+
+        self.create_Inscription()
+        self.create_Connexion()
 
 
-def application(self):
-    loader = QUiLoader()
-    application = loader.load("Application.ui", None)
-    #window.pushButton.clicked.connect(lambda: print("OUI"))
+        button_box = QDialogButtonBox(QDialogButtonBox.Cancel)
 
-    application.show()
+        button_box.rejected.connect(self.reject)
+
+        main_layout = QVBoxLayout()
+        main_layout.setMenuBar(self._menu_bar)
+        main_layout.addWidget(self._Inscription)
+        main_layout.addWidget(self._Connexion)
+
+
+        main_layout.addWidget(button_box)
+
+        self.setLayout(main_layout)
+        self.setWindowTitle("CrowdSaucing")
+
+
+
+
+    def create_Inscription(self):
+        self._Inscription = QGroupBox("Première connexion")
+        button = QPushButton("Inscription")
+        button.clicked.connect(self.create_User)
+        layout = QVBoxLayout()
+        layout.addWidget(button)
+
+
+        self._Inscription.setLayout(layout)
+
+
+    def create_Connexion(self):
+        self._Connexion = QGroupBox("Déjà inscrit ?")
+        button = QPushButton("Connexion")
+        button.clicked.connect(self.connect_User)
+        layout = QVBoxLayout()
+        layout.addWidget(button)
+
+
+        self._Connexion.setLayout(layout)
+
+
+    def create_User(self):
+
+        self._InscriptionMenu = QGroupBox("Inscription")
+
+        self._Name = QLineEdit("Enter your name")
+        self._Password = QLineEdit("Enter your password")
+
+        button = QPushButton("Signup")
+        button.clicked.connect(self.InscriptionDB)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self._Name)
+        layout.addWidget(self._Password)
+        layout.addWidget(button)
+
+        self._InscriptionMenu.setLayout(layout)
+
+        self._InscriptionMenu.show()
+
+        print("Inscription")
+
+    def connect_User(self):
+        self._ConnexionMenu = QGroupBox("Connexion")
+
+        self._Name = QLineEdit("Enter your nom")
+        self._Password = QLineEdit("Enter you password")
+
+        button = QPushButton("Connect")
+        button.clicked.connect(self.ConnexionDB)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self._Name)
+        layout.addWidget(self._Password)
+        layout.addWidget(button)
+
+        self._ConnexionMenu.setLayout(layout)
+
+        self._ConnexionMenu.show()
+
+
+    def InscriptionDB(self):
+
+        self.name = self._Name.text()
+        self.password = self._Password.text()
+
+        self._InscriptionMenu.close()
+
+        print(self.name, self.password)
+
+
+    def ConnexionDB(self):
+
+        self.name = self._Name.text()
+        self.password = self._Password.text()
+
+        self._ConnexionMenu.close()
+
+        print(self.name, self.password)
 
 
 
@@ -44,13 +141,7 @@ def application(self):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-
-    loader = QUiLoader()
-    window = loader.load("menu.ui", None)
-    window.pushButton.clicked.connect(connect_User)
-    window.pushButton_2.clicked.connect(create_User)
-    #window.pushButton_3.clicked.connect(leave)
-
+    window = Dialog()
     window.show()
 
     exit(app.exec_())
